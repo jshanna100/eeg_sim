8027142bbfd73501442530707252ab8f5fd6c4d0
jshanna100 <jeff.hanna@gmail.com>
2021-10-21 18:02:11 +0200

    switching to "distributed" source localisation for saccades

--- eeg_sim/sacc_loc.py
+++ eeg_sim/sacc_loc.py
@@ -10,7 +10,7 @@ elif isdir("/home/hannaj/"):
     base_dir = "/home/hannaj/"
 eeg_dir = base_dir+"hdd/memtacs/proc/reog/"
 epo = mne.read_epochs("{}grand_saccade-epo.fif".format(eeg_dir))
-epo.set_eeg_reference()
+#epo.set_eeg_reference()
 evo = epo.average()
 
 # fit sphere
@@ -25,9 +25,18 @@ exg_rr /= np.sqrt(np.sum(exg_rr * exg_rr, axis=1, keepdims=True))
 exg_rr *= 0.96 * R
 exg_rr += r0
 
+# random orientations; will be allowed to vary with localisation anyway
+exg_nn = np.random.randn(*exg_rr.shape)
+exg_nn = (exg_nn.T / np.linalg.norm(exg_nn, axis=1)).T
+
+src = mne.setup_volume_source_space(pos={"rr":exg_rr, "nn":exg_nn},
+                                    sphere_units="mm")
+fwd = mne.make_forward_solution(evo.info, None, src, sphere, meg=False,
+                                eeg=True, n_jobs=2)
 # covariance
 cov = mne.compute_covariance(epo)
 
-# dipoles
-dip_0, resid_0 = mne.fit_dipole(evo, cov, sphere, pos=exg_rr[0,], min_dist=0)
-dip_1, resid_1 = mne.fit_dipole(evo, cov, sphere, pos=exg_rr[1,], min_dist=0)
+inv = mne.minimum_norm.make_inverse_operator(evo.info, fwd, cov, fixed=False)
+evo.set_eeg_reference(projection=True)
+stc, resid = mne.minimum_norm.apply_inverse(evo, inv, method="sLORETA",
+                                            pick_ori="vector")
