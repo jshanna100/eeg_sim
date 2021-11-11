84e7e51f24968e704b9d99a86ae8dff6bc347e30
jshanna100 <jeff.hanna@gmail.com>
2021-11-11 15:31:19 +0100

    b

--- eeg_sim/sacc_loc.py
+++ eeg_sim/sacc_loc.py
@@ -2,10 +2,13 @@ from os.path import isdir
 import numpy as np
 import mne
 from mne.simulation.raw import _SimForwards, _check_head_pos
+from mne.minimum_norm.inverse import (_check_or_prepare, _assemble_kernel,
+                                      _pick_channels_inverse_operator)
 import matplotlib.pyplot as plt
 import pickle
 plt.ion()
-from mayavi.mlab import points3d, plot3d, mesh, figure
+from mayavi import mlab
+from mayavi.mlab import points3d, plot3d, mesh, quiver3d, figure
 
 def get_rand_rrs(size, R, r0, z_excl=0.):
     r_rr = np.zeros(size)
@@ -50,12 +53,17 @@ if isdir("/home/jev"):
 elif isdir("/home/hannaj/"):
     base_dir = "/home/hannaj/"
 eeg_dir = base_dir+"hdd/memtacs/proc/reog/"
+img_dir = eeg_dir+"anim/"
 
 loc_time = (-0.0125, 0.0125)
 loc_time = (-0.006, -0.002)
 # loc_time = (0.002, 0.006)
 loc_time = (-0.01, 0.01)
 loc_resid = False
+animate = True
+
+snr = 3.0  # Standard assumption for average data but using it for single trial
+lambda2 = 1.0 / snr ** 2
 
 try:
     evo = mne.read_evokeds("{}grand_saccade-ave.fif".format(eeg_dir))[0]
@@ -69,7 +77,9 @@ except:
     evo.save("{}grand_saccade-ave.fif".format(eeg_dir))
     cov.save("{}grand_saccade-cov.fif".format(eeg_dir))
 
+epo = mne.read_epochs("{}grand_saccade-epo.fif".format(eeg_dir))
 evo.crop(tmin=loc_time[0], tmax=loc_time[1])
+epo.crop(tmin=loc_time[0], tmax=loc_time[1])
 
 info, times, first_samp = evo.info, evo.times, 0
 
@@ -98,13 +108,13 @@ dev_head_ts, offsets = _check_head_pos(None, info, first_samp, times)
 get_fwd = _SimForwards(dev_head_ts, offsets, info, None, src, sphere, 0.005,
                        8, mne.pick_types(info, eeg=True))
 fwd = next(get_fwd.iter)
-inv = mne.minimum_norm.make_inverse_operator(evo.info, fwd, cov,
+inv = mne.minimum_norm.make_inverse_operator(epo.info, fwd, cov,
                                              fixed=False, depth=0.8)
 
-stc, resid, expl = mne.minimum_norm.apply_inverse(evo, inv, method="sLORETA",
-                                                  pick_ori="vector",
-                                                  return_residual=True)
-print("\n\n\nEye variance: {}\n\n\n".format(expl))
+stc = mne.minimum_norm.apply_inverse_epochs(epo, inv, lambda2,
+                                                   method="sLORETA",
+                                                   pick_ori="vector")
+#print("\n\n\nEye variance: {}\n\n\n".format(expl))
 
 expls = []
 rand_rrs = []
@@ -139,12 +149,55 @@ for shuf_idx in range(0):
 expls = np.array(expls)
 rand_rrs = np.array(rand_rrs)
 
-fig = figure()
-draw_eeg(evo.info, 0.01, (0,0,0), fig)
-# get all rrs that performed better than simple eye model
-better_inds = expls > expl
-for bi in np.where(better_inds)[0]:
-    draw_pair(rand_rrs[bi,], 0.005, (1,0,0), fig)
+## visualise variance of dipole vecs
+# for tractability and visualisation, grab just a subset
+alpha = 0.008
+epo_inds = np.random.randint(len(stc), size=5000)
+vecs = np.zeros((len(epo_inds), *stc[0].data.shape))
+for vec_idx, epo_idx in enumerate(epo_inds):
+     vecs[vec_idx,] = stc[epo_idx].data
+fig, axes = plt.subplots(2, 3)
+axes[0,0].plot(vecs[:, 0, 0,].T, color="red", alpha=alpha)
+axes[0,1].plot(vecs[:, 0, 1,].T, color="blue", alpha=alpha)
+axes[0,2].plot(vecs[:, 0, 2,].T, color="green", alpha=alpha)
+axes[1,0].plot(vecs[:, 1, 0,].T, color="red", alpha=alpha)
+axes[1,1].plot(vecs[:, 1, 1,].T, color="blue", alpha=alpha)
+axes[1,2].plot(vecs[:, 1, 2,].T, color="green", alpha=alpha)
+for axe in axes:
+    for ax in axe:
+        ax.set_ylim(-1.5, 1.5)
 
-draw_pair(exg_rr, 0.01, (0,0,1), fig)
-draw_sphere(R, r0, (0,0,1), 0.1, fig)
+# # calculate explained, resids, percent of data exlained, etc
+# this_inv = _check_or_prepare(inv, 1, lambda2, "sLORETA", None, None)
+# K, noise_norm, vertno, source_nn = _assemble_kernel(this_inv, None, "sLORETA",
+#                                                     "vector", use_cps=True)
+
+#
+# if animate:
+#     samp_n = stc.data.shape[-1]
+#     vecs = stc.data / np.linalg.norm(stc.data, axis=1, keepdims=True)
+#     vecs *= R
+#     for samp_idx in range(samp_n):
+#         fig = figure(size=(1920, 1920))
+#         draw_eeg(evo.info, 0.01, (0,0,0), fig)
+#         draw_sphere(R, r0, (0,0,1), 0.1, fig)
+#         quiver3d(exg_rr[0,0], exg_rr[0,1], exg_rr[0,2],
+#                  vecs[0,0,samp_idx], vecs[0,1,samp_idx], vecs[0,2,samp_idx],
+#                  scale_factor=R*5, figure=fig)
+#         quiver3d(exg_rr[1,0], exg_rr[1,1], exg_rr[1,2],
+#                  vecs[1,0,samp_idx], vecs[1,1,samp_idx], vecs[1,2,samp_idx],
+#                  scale_factor=R*5, figure=fig)
+#         mlab.view(azimuth=87, elevation=20, distance=0.722,
+#                   focalpoint=[0.0021, -0.012,  0.012],
+#                   reset_roll=False, figure=fig)
+#         mlab.savefig("{}_{}.tiff".format(img_dir, samp_idx))
+#
+# else:
+#     fig = figure()
+#     draw_eeg(evo.info, 0.01, (0,0,0), fig)
+#     draw_sphere(R, r0, (0,0,1), 0.1, fig)
+#     # get all rrs that performed better than simple eye model
+#     better_inds = expls > expl
+#     for bi in np.where(better_inds)[0]:
+#         draw_pair(rand_rrs[bi,], 0.005, (1,0,0), fig)
+#     draw_pair(exg_rr, 0.01, (0,0,1), fig)
